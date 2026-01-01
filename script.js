6720d5ffc53c1ee1a0973fbc4559e6d39c09e1fb
sulemanbhai21 <sohelbhai9376@gmail.com>
2026-01-01 14:57:30 +0530

    Registation Form Code

---
+++ script.js
@@ -0,0 +1,24 @@
+document.getElementById("regForm").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+    let confirmPassword = document.getElementById("confirmPassword").value;
+    let message = document.getElementById("message");
+
+    if (name === "" || email === "" || password === "" || confirmPassword === "") {
+        message.style.color = "red";
+        message.innerText = "All fields are required!";
+        return;
+    }
+
+    if (password !== confirmPassword) {
+        message.style.color = "red";
+        message.innerText = "Passwords do not match!";
+        return;
+    }
+
+    message.style.color = "green";
+    message.innerText = "Registration Successful!";
+});
